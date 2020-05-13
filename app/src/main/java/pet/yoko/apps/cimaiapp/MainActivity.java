9844a6a82e09910c13383903682a46cb7b35afcd
Rafael Perzzzo <rafaelperazzo@gmail.com>
2020-05-13 10:12:37 -0300

    Primeiro commit

---
+++ app/src/main/java/pet/yoko/apps/cimaiapp/MainActivity.java
@@ -0,0 +1,18 @@
+package pet.yoko.apps.cimaiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Spinner spinner = (Spinner) findViewById(R.id.cmbAno);
+
+    }
+}
