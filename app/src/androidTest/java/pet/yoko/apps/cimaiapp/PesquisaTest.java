0bbd54f1999fc312fa08161ca77b73d9c49bed3f
Rafael Perzzzo <rafaelperazzo@gmail.com>
2020-05-25 11:29:50 -0300

    Atualizado 2020-05-25 11:29:50

--- app/src/androidTest/java/pet/yoko/apps/cimaiapp/PesquisaTest.java
+++ app/src/androidTest/java/pet/yoko/apps/cimaiapp/PesquisaTest.java
@@ -16,11 +16,14 @@ import org.junit.Test;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
+import java.util.Random;
+
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -34,6 +37,14 @@ import static org.hamcrest.Matchers.not;
 
 public class PesquisaTest {
     private IdlingResource mIdlingResource;
+    Random gerador = new Random();
+
+    public void repetir(int id) {
+        for (int i = 0; i<=8; i++) {
+            onView(withId(id)).perform(click());
+            onData(anything()).atPosition(gerador.nextInt(8)).perform(click());
+        }
+    }
 
     @Rule
     public ActivityTestRule<PesquisaActivity> activityRule
@@ -56,22 +67,26 @@ public class PesquisaTest {
     @Test
     public void testAbrirPesquisaProducoes() {
         onView(withText("PRODUÇÃO BIBLIOGRÁFICA")).perform(click());
-        onView(withId(R.id.btnArea)).perform(click());
-        onView(withId(R.id.spinAno)).perform(click());
-        onData(anything()).atPosition(1).perform(click());
-        onView(withId(R.id.spinAno)).check(matches(withSpinnerText(containsString("2014"))));
-        onView(withId(R.id.spinAno)).perform(click());
-        onData(anything()).atPosition(2).perform(click());
-        onView(withId(R.id.spinAno)).perform(click());
-        onData(anything()).atPosition(3).perform(click());
-
+        repetir(R.id.spinAno);
     }
 
     @Test
-    public void testAbrirPesquisaProducoesPorProducao() {
+    public void testAbrirPesquisaProducoesBotoes() {
         onView(withText("PRODUÇÃO BIBLIOGRÁFICA")).perform(click());
-        onView(withId(R.id.btnPorProducao)).perform(click());
-        pressBack();
+        for (int i = 0; i<=3; i++) {
+            onView(withId(R.id.spinAno)).perform(click());
+            onData(anything()).atPosition(gerador.nextInt(8)).perform(click());
+            onView(withId(R.id.btnPorProducao)).perform(click());
+            onView(withId(R.id.porProducoesChart)).check(matches(isDisplayed()));
+            pressBack();
+            onView(withId(R.id.btnGrandeArea)).perform(click());
+            onView(withId(R.id.porProducoesChart)).check(matches(isDisplayed()));
+            pressBack();
+            onView(withId(R.id.btnBolsistas)).perform(click());
+            onView(withId(R.id.porProducoesChart)).check(matches(isDisplayed()));
+            pressBack();
+            onView(withId(R.id.btnArea)).perform(click());
+        }
     }
 
     @Test
@@ -79,27 +94,56 @@ public class PesquisaTest {
         onView(withText("PRODUÇÃO BIBLIOGRÁFICA")).perform(click());
         onView(withId(R.id.spinAno)).perform(click());
         onData(anything()).atPosition(0).perform(click());
-        onView(withId(R.id.txtPeriodicos)).check(matches((not(withText("0")))));
-        onView(withId(R.id.txtLivros)).check(matches((not(withText("0")))));
-        onView(withId(R.id.txtAnais)).check(matches((not(withText("0")))));
-        onView(withId(R.id.txtCapitulos)).check(matches((not(withText("0")))));
+        for (int i = 0; i<=8; i++) {
+            onView(withId(R.id.spinAno)).perform(click());
+            onData(anything()).atPosition(gerador.nextInt(8)).perform(click());
+            onView(withId(R.id.txtPeriodicos)).check(matches((not(withText("0")))));
+            onView(withId(R.id.txtLivros)).check(matches((not(withText("0")))));
+            onView(withId(R.id.txtAnais)).check(matches((not(withText("0")))));
+            onView(withId(R.id.txtCapitulos)).check(matches((not(withText("0")))));
+        }
+
     }
 
     @Test
-    public void testAbrirPesquisaGrupos() {
+    public void testAbrirPesquisaGrupos(){
         onView(withText("GRUPOS DE PESQUISA")).perform(click());
+        onView(withId(R.id.btnGrupoListaProjetos)).perform(click());
+        onView(withId(R.id.btnGrupoArea)).perform(click());
+        onView(withId(R.id.txtGrupoCertificados)).check(matches((not(withText("0")))));
+        onView(withId(R.id.txtGrupoDocentes)).check(matches((not(withText("0")))));
+        onView(withId(R.id.txtGrupoTecnicos)).check(matches((not(withText("0")))));
+        onView(withId(R.id.txtGrupoDiscentes)).check(matches((not(withText("0")))));
     }
 
     @Test
     public void testAbrirPesquisaProjetos() {
         onView(withText("PROJETOS DE PESQUISA")).perform(click());
-        onView(withId(R.id.spinProjetoAno)).perform(click());
-        onData(anything()).atPosition(1).perform(click());
-        onView(withId(R.id.spinProjetoAno)).perform(click());
-        onData(anything()).atPosition(2).perform(click());
-        onView(withId(R.id.spinProjetoAno)).perform(click());
-        onData(anything()).atPosition(3).perform(click());
+        for (int i = 0; i<=8; i++) {
+            onView(withId(R.id.spinProjetoAno)).perform(click());
+            onData(anything()).atPosition(gerador.nextInt(8)).perform(click());
+            onView(withId(R.id.txtProjetos)).check(matches((not(withText("0")))));
+            onView(withId(R.id.txtProjetoCoordenadores)).check(matches((not(withText("0")))));
+            onView(withId(R.id.txtProjetoDiscentes)).check(matches((not(withText("0")))));
+        }
 
+    }
+
+    @Test
+    public void testAbrirPesquisaProjetosBotoes() {
+        onView(withText("PROJETOS DE PESQUISA")).perform(click());
+        for (int i = 0; i<=3; i++) {
+            onView(withId(R.id.spinProjetoAno)).perform(click());
+            onData(anything()).atPosition(gerador.nextInt(8)).perform(click());
+            onView(withId(R.id.btnProjetoLista)).perform(click());
+            onView(withId(R.id.btnProjetoGrandeArea)).perform(click());
+            onView(withId(R.id.porProducoesChart)).check(matches(isDisplayed()));
+            pressBack();
+            onView(withId(R.id.btnProjetoPorAno)).perform(click());
+            onView(withId(R.id.porProducoesChart)).check(matches(isDisplayed()));
+            pressBack();
+            onView(withId(R.id.btnProjetoArea)).perform(click());
+        }
     }
 
     @After
