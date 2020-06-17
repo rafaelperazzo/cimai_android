c30834be04155ab08be83024fb2916deddf8c93d
Rafael Perzzzo <rafaelperazzo@gmail.com>
2020-06-17 13:34:45 -0300

    Atualizado 2020-06-17 13:34:45

---
+++ app/src/main/java/pet/yoko/apps/cimaiapp/ai/AIActivity.java
@@ -0,0 +1,16 @@
+package pet.yoko.apps.cimaiapp.ai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pet.yoko.apps.cimaiapp.R;
+
+public class AIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_i);
+    }
+}
